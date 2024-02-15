d016600a0dd50d4a5606ec4f234efd1cdf0af8a3
errere <www@skjsajs.ashakjs>
2024-02-15 18:05:04 -0500

    Estructurando el proyecto API REST. Index, Modelos y controladores

---
+++ index.js
@@ -0,0 +1,27 @@
+const usuarios = require('./controllers/usuarios');
+const cursos = require('./controllers/cursos')
+
+
+
+const express = require ('express');
+const mongoose = require ('mongoose');
+
+//conexion a la base de datos de mongodb
+mongoose.connect('mongodb://localhost:27017/usercoursesdb', {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(() => console.log('Conectado a mongoDB...'))
+    .catch(err => console.log ('no se pudo conectar con mongoDB..', err));
+
+
+// middleware
+const app =express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// end points (recursos)
+app.use('/api/usuarios', usuarios);
+app.use('/api/cursos', cursos);
+
+const port = process.env.PORT || 84;
+app.listen(port, () => {
+    console.log('Api REST Ok, y ejecutandose')
+})
